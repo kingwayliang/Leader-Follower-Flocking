2310c804baba621b673cea63f4dd21f1c720371d
Alan <liuhy14@stanford.edu>
2020-03-09 15:23:51 -0700

    added controller

--- Visualization.py
+++ Visualization.py
@@ -67,6 +67,54 @@ class Visualizer:
         self.window.mainloop()
 
 
+class Robots:
+    def __init__(self):
+        self.robots = np.array([[0.0, 0],
+                                [1, 2],
+                                [2, 2],
+                                [2, 1],
+                                [0, 1],
+                                [-1, -1],
+                                [-1, -2],
+                                [-2, -2],
+                                [-2, -3],
+                                [-1, -3]])
+        self.followers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.leaders = [0]
+        self.timestep = 0
+        self.time_interval = 0.05
+        self.d1 = 0.1
+        self.d2 = 0
+        self.ka = 1
+        self.ks = 0.2
+        self.show()
+
+    def update(self, leaders_displacement=None):
+        num_robots = self.robots.shape[0]
+        num_followers = len(self.followers)
+        num_leaders = len(self.leaders)
+        if leaders_displacement is not None:
+            for i in range(num_leaders):
+                self.robots[self.leaders[i]] += leaders_displacement[i]
+        dist_mat = np.linalg.norm(self.robots - np.expand_dims(self.robots, axis=1), axis=2)
+        control_input = np.zeros((num_robots, 2))
+        for f in self.followers:
+            for i in range(num_robots):
+                d = dist_mat[f, i] 
+                if f != i:
+                    control_input[f] += (-self.ka + self.ks / (d - self.d1) ** 2 / d) * (self.robots[f] - self.robots[i])
+        for f in self.followers:
+            self.robots[f] += control_input[f] * self.time_interval
+        self.show()
+    
+    def show(self):
+        for f in self.followers:
+            plt.scatter(self.robots[f, 0], self.robots[f, 1], color='b')
+        for l in self.leaders:
+            plt.scatter(self.robots[l, 0], self.robots[l, 1], color='r')
+        plt.show()
+
+
 if __name__ == "__main__":
     vis = Visualizer(None)
     vis.run()
